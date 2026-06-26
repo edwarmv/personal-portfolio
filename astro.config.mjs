ee3ad353fc1a1fc5a50a6524f79c491f570d9a67
Edwar Martinez <edwar_kenedy@hotmail.com>
2026-06-26 00:05:34 -0400

    Add node adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.edwarmv.com",
+  adapter: node({
+    mode: "standalone",
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
